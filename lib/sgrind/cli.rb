44175ee594894c7bf18d20e9f791558519af1dd0
Sushi Chanrai <sushi@pathworks.co>
2018-08-11 08:59:32 +0000

    list event deets by location method version 1

--- lib/sgrind/cli.rb
+++ lib/sgrind/cli.rb
@@ -1,10 +1,12 @@
 class Cli
 
+
 @@locations_array = []
 
 #test all the display methods
 def display_event_index
   i = 0
+  puts "************* All Events *************"
   puts '------------------------------'
   Event.all.each do |event|
     puts "#{i += 1}. #{event.name} | #{event.location}"
@@ -16,6 +18,7 @@ end
 #speakers turning from string to array line 18 is untested
 def display_event_details(event)
     x = event.speakers * ", "
+    puts "************* Event Details *************"
   puts '------------------------------'
     puts "Title: #{event.name}"
     puts "Date: #{event.date}"
@@ -29,25 +32,27 @@ def display_event_details(event)
 end
 
 
-=begin
+
+#location mini-menu
 def display_events_by_location
  #use Event.fetch_by_location(location)
 end
 
-=end
 
 
 
 def display_event_locations
   i = 0
   d = Event.list_locations
+  puts "************* Event Locations *************"
   d.each do|location|
-  @@locations_array << location
+  @@locations_array << location #you need this for fetch_by_location
   puts "#{i += 1}. #{location}"
   end
 end
 
 
+#you need this for fetch_by_location
   def self.locations
     @@locations_array
   end
--- lib/sgrind/events.rb
+++ lib/sgrind/events.rb
@@ -18,29 +18,40 @@ class Event
      end
   end
 
- #needs to be called on an event instance and takes in a hash from the scrape_details_page(url) method
-   def add_details(scraped_details)
-     scraped_details.each do |attr, value|
-       self.send("#{attr}=", value)
-     end
-    self
-   end
-
-
-   def self.list_locations
-     self.all.map do | event|
-        event.location
-      end.uniq.sort
+ #needs to be called on an event instance and takes in a hash from the Scraper > scrape_details_page(url) method
+  def add_details(scraped_details)
+    scraped_details.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+   self
   end
 
 
-    #location will be = CLI.locations[locations_array_index]
+  def self.list_locations
+    self.all.map do | event|
+      event.location
+    end.uniq.sort
+  end
+
+
+  #argument will be a single string location eg. "Tbilisi,GE" which is a result of CLI.locations[locations_array_index]
+  #output will be array of event objects that match query
   def self.fetch_by_location(location)
     self.all.select {|event| event.location == location}
   end
 
-  #make fetch event detials_link
 
+  #fetches event details_link by event id
+  def self.fetch_details_url(event_id)
+    self.all[event_id].details_link
+  end
+
+  #input will be array from fetch_by_location method
+  def self.list_event_details_by_location(filtered_event_array)
+    filter_deets_urls = filtered_event_array.map {| element | element.details_link}
+    c = filter_deets_urls.each { |url| Scraper.scrape_details_page(url) }
+    binding.pry
+  end
 
   def self.all
       @@all
